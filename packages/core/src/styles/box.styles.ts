c7f58aef1b15026865d42b1ea1492c584d057afb
Muhammad Rizki A <dev.muhammadrizki@gmail.com>
2026-01-04 12:52:34 +0700

    fixing

--- packages/core/src/styles/box.styles.ts
+++ packages/core/src/styles/box.styles.ts
@@ -10,7 +10,7 @@ export const boxVariants = cva(
           "bg-(--color)/20 [--tw-shadow-color:var(--color)]/10 border-(--color)/10 text-(--color)",
       },
       variant: {
-        default: "bg-background border-(--color-foreground)/10",
+        default: "bg-background dark:bg-foreground/10 border-foreground/10",
         primary: "[--color:var(--color-primary)]",
         secondary: "[--color:var(--color-foreground)]",
         success: "[--color:var(--color-success)]",
@@ -21,12 +21,12 @@ export const boxVariants = cva(
       raised: {
         single: [
           "mb-2.5",
-          "before:inset-x-2 before:h-2.5 before:bg-(--color)/20 before:-bottom-2.5 before:rounded-b-xl before:border-x before:border-b before:border-(--color)/10 before:z-[-1] before:shadow-md/5 before:opacity-60",
+          "before:inset-x-2.5 before:h-[10px] before:bg-(--color)/20 before:-bottom-[11px] before:rounded-b-xl before:border-x before:border-b before:border-(--color)/10 before:z-[-1] before:shadow-md/5 before:opacity-60",
         ],
         double: [
           "mb-4",
-          "before:inset-x-2 before:h-2.5 before:bg-(--color)/20 before:-bottom-2.5 before:rounded-b-xl before:border-x before:border-b before:border-(--color)/10 before:z-[-1] before:shadow-md/5 before:opacity-60",
-          "after:inset-x-4.5 after:h-[0.5rem] after:bg-(--color)/20 after:-bottom-[1.1rem] after:rounded-b-xl after:border-x after:border-b after:border-(--color)/10 after:z-[-1] after:shadow-md/5 after:opacity-40",
+          "before:inset-x-2.5 before:h-[10px] before:bg-(--color)/20 before:-bottom-[11px] before:rounded-b-xl before:border-x before:border-b before:border-(--color)/10 before:z-[-1] before:shadow-md/5 before:opacity-60",
+          "after:inset-x-5 after:h-[8px] after:bg-(--color)/20 after:-bottom-[19px] after:rounded-b-xl after:border-x after:border-b after:border-(--color)/10 after:z-[-1] after:shadow-md/5 after:opacity-40",
         ],
       },
     },
@@ -35,13 +35,13 @@ export const boxVariants = cva(
         filled: false,
         variant: "default",
         class:
-          "before:border-(--color-foreground)/10 after:border-(--color-foreground)/10",
+          "before:border-foreground/10 dark:before:bg-foreground/10 after:border-foreground/10 dark:after:bg-foreground/10",
       },
       {
         filled: true,
         variant: "default",
         class:
-          "bg-(--color-foreground)/25 before:bg-(--color-foreground)/10 before:border-(--color-foreground)/20 after:bg-(--color-foreground)/10 after:border-(--color-foreground)/20 border-transparent text-foreground",
+          "bg-foreground/25 dark:bg-foreground/30 before:bg-foreground/10 before:border-foreground/20 after:bg-foreground/10 after:border-foreground/20 border-transparent text-foreground",
       },
       {
         filled: true,
