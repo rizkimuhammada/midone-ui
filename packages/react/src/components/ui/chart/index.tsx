c2530df61614ed55c2f7b9bffad03045bc8fe5b0
Muhammad Rizki A <dev.muhammadrizki@gmail.com>
2026-02-15 11:05:19 +0700

    feat: Update chart

--- packages/react/src/components/ui/chart/index.tsx
+++ packages/react/src/components/ui/chart/index.tsx
@@ -2,6 +2,7 @@ import ChartJs from "chart.js/auto";
 import { useRef, useEffect } from "react";
 import { cn } from "@midoneui/core/utils/cn";
 import { chart } from "@midoneui/core/styles/chart.styles";
+import type { ChartType, ChartConfiguration } from "chart.js";
 
 function getColor(name: string, opacity = 1) {
   const color = getComputedStyle(document.documentElement)
@@ -15,24 +16,28 @@ function getColor(name: string, opacity = 1) {
   return color;
 }
 
-function Chart({
+function Chart<TType extends ChartType = ChartType>({
   className,
   config,
+  getRef,
   ...props
 }: React.ComponentProps<"canvas"> & {
-  config: ConstructorParameters<typeof ChartJs>[1];
+  config: ChartConfiguration<TType>;
+  getRef?: (chart: ChartJs<TType>) => void;
 }) {
   const chartRef = useRef<
-    HTMLCanvasElement & {
-      instance?: {};
-    }
+    | (HTMLCanvasElement & {
+        instance?: ChartJs<TType>;
+      })
+    | null
   >(null);
 
   useEffect(() => {
     if (chartRef.current && !chartRef.current.instance) {
       chartRef.current.instance = new ChartJs(chartRef.current, config);
+      getRef?.(chartRef.current.instance);
     }
-  }, []);
+  }, [config, getRef]);
 
   return <canvas className={cn(chart, className)} ref={chartRef} {...props} />;
 }
