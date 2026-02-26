de91c4f0c8fd05c3f763053ab0cd629e1726b23e
Muhammad Rizki A <dev.muhammadrizki@gmail.com>
2026-02-26 11:03:37 +0700

    feat: fix radio group spacing issue

--- packages/core/src/styles/radio-group.styles.ts
+++ packages/core/src/styles/radio-group.styles.ts
@@ -1,7 +1,7 @@
 export const radioGroupRoot = "flex flex-col gap-2.5";
 export const radioGroupLabel = "";
 export const radioGroupIndicator =
-  "left-(--left) top-(--top) h-(--height) size-4 [&>svg]:absolute [&>svg]:inset-0 [&>svg]:m-auto [&>svg]:size-3 [&>svg]:stroke-10";
+  "left-(--left) top-(--top) h-(--height) w-4 [&>svg]:absolute [&>svg]:inset-0 [&>svg]:m-auto [&>svg]:size-3 [&>svg]:stroke-10";
 export const radioGroupItem = "relative flex items-center gap-3";
 export const radioGroupItemText = "";
 export const radioGroupItemControl =
--- packages/vue/src/pages/Field.vue
+++ packages/vue/src/pages/Field.vue
@@ -32,13 +32,10 @@ import * as select from "@zag-js/select";
 import { Textarea } from "@/components/ui/textarea";
 import {
   RadioGroupRoot,
-  RadioGroupLabel,
   RadioGroupItem,
-  RadioGroupItemText,
   RadioGroupItemControl,
 } from "@/components/ui/radio-group";
 
-const frameworks = ["React", "Solid", "Vue", "Svelte"];
 const comboboxData = [
   { label: "React", code: "react" },
   { label: "Solid", code: "solid" },
