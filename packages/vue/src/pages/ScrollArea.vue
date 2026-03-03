14704260bdaeba91ac0b454551bcd89d20050fa9
Dwi Company <dwicompany@Left4code.local>
2026-03-03 21:54:50 +0700

    feat(scroll-area): refine scrollbar styles and clean up usage page

--- packages/vue/src/pages/ScrollArea.vue
+++ packages/vue/src/pages/ScrollArea.vue
@@ -7,14 +7,6 @@ import {
   ScrollAreaThumb,
   ScrollAreaCorner,
 } from "@/components/ui/scroll-area";
-
-import * as scrollArea from "@zag-js/scroll-area"
-  import { useMachine, normalizeProps } from "@zag-js/vue"
-  import { computed, useId } from "vue"
-
-  const service = useMachine(scrollArea.machine, { id: useId() })
-
-  const api = computed(() => scrollArea.connect(service, normalizeProps))
 </script>
 
 <template>
@@ -29,32 +21,17 @@ import * as scrollArea from "@zag-js/scroll-area"
               <div class="text-xl font-medium mb-4">Scroll Area Example</div>
               <div v-for="i in 20" :key="i" class="mb-4 last:mb-0 opacity-80">
                 This is line number {{ i }} of the scrollable content. It helps
-                demonstrate how the custom scrollbar works within the
-                Midone UI system.
+                demonstrate how the custom scrollbar works within the Midone UI
+                system.
               </div>
             </ScrollAreaContent>
           </ScrollAreaViewport>
           <ScrollAreaScrollbar>
-      <ScrollAreaThumb />
-    </ScrollAreaScrollbar>
-    <ScrollAreaCorner />
+            <ScrollAreaThumb />
+          </ScrollAreaScrollbar>
+          <ScrollAreaCorner />
         </ScrollAreaRoot>
       </div>
     </div>
   </div>
-  <!-- <div v-bind="api.getRootProps()" class="h-72 w-70">
-    <div v-bind="api.getViewportProps()" class="h-full [scrollbar-width:none] [&::-webkit-scrollbar]:hidden">
-      <div v-bind="api.getContentProps()">
-        <div class="text-xl font-medium mb-4">Scroll Area Example</div>
-              <div v-for="i in 20" :key="i" class="mb-4 last:mb-0 opacity-80">
-                This is line number {{ i }} of the scrollable content. It helps
-                demonstrate how the custom scrollbar works within the
-                Midone UI system.
-              </div>
-      </div>
-    </div>
-    <div v-bind="api.getScrollbarProps()">
-      <div v-bind="api.getThumbProps()" />
-    </div>
-  </div> -->
 </template>
