430dbb1b5427a2dfc150ffff72108dde0b45fcb2
Dwi Company <dwicompany@Left4code.local>
2026-03-04 00:43:23 +0700

    style(core): update scroll-area thumb size

--- packages/core/src/styles/scroll-area.styles.ts
+++ packages/core/src/styles/scroll-area.styles.ts
@@ -5,8 +5,8 @@ export const scrollAreaScrollbar = [
     "before:content-[''] before:absolute",
     "data-[scrolling]:duration-[0ms]",
     "data-[hover]:opacity-100 data-[scrolling]:opacity-100 data-[hover]:pointer-events-auto data-[scrolling]:pointer-events-auto",
-    "data-[orientation=vertical]:w-2.5 data-[orientation=vertical]:before:w-5 data-[orientation=vertical]:before:h-full data-[orientation=vertical]:before:left-1/2 data-[orientation=vertical]:before:-translate-x-1/2 data-[orientation=vertical]:[&:not([data-overflow-y])]:hidden",
-    "data-[orientation=horizontal]:h-2.5 data-[orientation=horizontal]:before:w-full data-[orientation=horizontal]:before:h-5 data-[orientation=horizontal]:before:left-0 data-[orientation=horizontal]:before:right-0 data-[orientation=horizontal]:before:bottom-[-0.5rem] data-[orientation=horizontal]:[&:not([data-overflow-x])]:hidden",
+    "data-[orientation=vertical]:w-2 data-[orientation=vertical]:before:w-5 data-[orientation=vertical]:before:h-full data-[orientation=vertical]:before:left-1/2 data-[orientation=vertical]:before:-translate-x-1/2 data-[orientation=vertical]:[&:not([data-overflow-y])]:hidden",
+    "data-[orientation=horizontal]:h-2 data-[orientation=horizontal]:before:w-full data-[orientation=horizontal]:before:h-5 data-[orientation=horizontal]:before:left-0 data-[orientation=horizontal]:before:right-0 data-[orientation=horizontal]:before:bottom-[-0.5rem] data-[orientation=horizontal]:[&:not([data-overflow-x])]:hidden",
 ];
 export const scrollAreaThumb =
     "w-full rounded-[inherit] bg-foreground/20 data-[orientation=horizontal]:w-auto data-[orientation=horizontal]:h-full";
